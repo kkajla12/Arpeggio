9704d7d1454e1b76632f6abe7eca065c7fa399d8
Marko Vojvodic <mvojvodic@ucla.edu>
2015-04-22 22:45:59 -0700

    Added simple home page.
    Added routes, controllers, and simple views for users.
    
    Added .travis.yml file for TravisCI.

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'application#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
