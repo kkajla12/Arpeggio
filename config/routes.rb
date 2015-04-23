da16cf357750fef27d3f86872bce5e9fc528a490
Marko Vojvodic <mvojvodic@ucla.edu>
2015-04-22 21:32:47 -0700

    Changed routes.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :products
 
   devise_for :users
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
