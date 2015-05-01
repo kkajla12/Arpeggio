768d071287ae65b4de64ecef08d487c1b0919e4d
Marko Vojvodic <mvojvodic@ucla.edu>
2015-04-30 23:56:33 -0700

    Added non-bootstrap styling.
    Added script to reset and seed db for testing purposes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @products = @user.products
   end
 
   # GET /users/new
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.delete_all
+Product.delete_all
+
 User.create(email: "user1@arpeggio.com", password: "password")
 User.create(email: "user2@arpeggio.com", password: "password")
 User.create(email: "user3@arpeggio.com", password: "password")
@@ -13,32 +16,32 @@ User.create(email: "user4@arpeggio.com", password: "password")
 User.create(email: "user5@arpeggio.com", password: "password")
 
 (0..4).each do |i|
-	Product.create!(name: "Marshall 2x12 Speaker Cabinet",
-		description: %{
-			This is a sample product listing for this particular product.
-			Why are you wasting time reading all of this? Don't you have
-			anything else to do? Go do something else please. Thank you.
-		},
-		price: 30.00, deposit: 150, classification: "speaker", user_id: (i + 1))
-	Product.create!(name: "Gibson SG Cherry Finish",
-		description: %{
-			This is a sample product listing for this particular product.
-			Why are you wasting time reading all of this? Don't you have
-			anything else to do? Go do something else please. Thank you.
-		},
-		price: 65.00, deposit: 350, classification: "guitar", user_id: (i + 1))
-	Product.create!(name: "Sennheiser E835S Performance Vocal Microphone",
-		description: %{
-			This is a sample product listing for this particular product.
-			Why are you wasting time reading all of this? Don't you have
-			anything else to do? Go do something else please. Thank you.
-		},
-		price: 15.00, deposit: 50, classification: "mic", user_id: (i + 1))
-	Product.create!(name: "Yamaha CP-300 88-Key Stage Piano",
-		description: %{
-			This is a sample product listing for this particular product.
-			Why are you wasting time reading all of this? Don't you have
-			anything else to do? Go do something else please. Thank you.
-		},
-		price: 80.00, deposit: 550, classification: "keyboard", user_id: (i + 1))
+  Product.create!(name: "Marshall 2x12 Speaker Cabinet",
+    description: %{
+      This is a sample product listing for this particular product.
+      Why are you wasting time reading all of this? Don't you have
+      anything else to do? Go do something else please. Thank you.
+    },
+    price: 30.00, deposit: 150, classification: "speaker", user_id: (i + 1))
+  Product.create!(name: "Gibson SG Cherry Finish",
+    description: %{
+      This is a sample product listing for this particular product.
+      Why are you wasting time reading all of this? Don't you have
+      anything else to do? Go do something else please. Thank you.
+    },
+    price: 65.00, deposit: 350, classification: "guitar", user_id: (i + 1))
+  Product.create!(name: "Sennheiser E835S Performance Vocal Microphone",
+    description: %{
+      This is a sample product listing for this particular product.
+      Why are you wasting time reading all of this? Don't you have
+      anything else to do? Go do something else please. Thank you.
+    },
+    price: 15.00, deposit: 50, classification: "mic", user_id: (i + 1))
+  Product.create!(name: "Yamaha CP-300 88-Key Stage Piano",
+    description: %{
+      This is a sample product listing for this particular product.
+      Why are you wasting time reading all of this? Don't you have
+      anything else to do? Go do something else please. Thank you.
+    },
+    price: 80.00, deposit: 550, classification: "keyboard", user_id: (i + 1))
 end
