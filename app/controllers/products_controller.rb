647a714fc3e023a2e8e65c54eecc4bdd91b73680
Marko Vojvodic <mvojvodic@ucla.edu>
2015-05-02 16:09:16 -0700

    Removed unneccessary routes and authentication for users. Cleaned up some of the html and css.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   # GET /products
