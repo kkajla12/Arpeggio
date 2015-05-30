17e03e7578968656c5cd5f342d3e8a0b0d5f4266
kkajla12 <kkajla12@gmail.com>
2015-05-30 15:35:56 -0700

    Added includes to product and order queries

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -123,7 +123,7 @@ class OrdersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_order
-      @order = Order.find(params[:id])
+      @order = Order.includes(:line_items).find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = User.includes(:products, :orders).find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
