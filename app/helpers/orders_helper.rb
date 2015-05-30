c72d18927b99af35a844326cb91cdd2f906842a3
kkajla12 <kkajla12@gmail.com>
2015-05-30 02:23:18 -0700

    Initial implementation of server-side-caching of:
    Products on products index page
    
    Orders on user's orders page

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,5 @@
 module OrdersHelper
+  def cache_key_for_order(order)
+    "order-#{order.id}-#{order.updated_at}"
+  end
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,8 @@
 module ProductsHelper
+  def cache_key_for_product(product)
+    "product-#{product.id}-#{product.updated_at}"
+  end
+  def cache_key_for_product_page
+    "product-page-#{Product.maximum(:updated_at)}"
+  end
 end
