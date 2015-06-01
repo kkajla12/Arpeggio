627863fdb687153954ed19468660cbee9833e5ce
kkajla12 <kkajla12@gmail.com>
2015-05-31 23:27:36 -0700

    fixed caching tag causing pagination error
    Added server-side-caching for:
    
    orders show page
    
    users show page

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,5 +1,2 @@
 module OrdersHelper
-  def cache_key_for_order(order)
-    "order-#{order.id}-#{order.updated_at}"
-  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def cache_key_for_product(product)
+    "product-#{product.id}-#{product.updated_at}"
+  end
+  def cache_key_for_order(order)
+    "order-#{order.id}-#{order.updated_at}"
+  end
+  def cache_key_for_user(user)
+    "user-#{user.id}-#{user.updated_at}"
+  end
 end
