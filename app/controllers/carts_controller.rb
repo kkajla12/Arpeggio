b18f3112f5f26ff4e0d19425e5b8da97416fa454
kkajla12 <kkajla12@gmail.com>
2015-06-02 00:19:49 -0700

    Added includes() to set_cart methods to preload line_items and products relations

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -67,7 +67,7 @@ class CartsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
-      @cart = Cart.includes(:line_items).find(params[:id])
+      @cart = Cart.includes(line_items: [:product]).find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
